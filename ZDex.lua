16ed833c959d3ff3186c127ddb8ba43531aa1993
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-29 00:34:37 +0700

    Add files via upload

--- ZDex.lua
+++ ZDex.lua
@@ -873,7 +873,107 @@ end
 
 	return {InitDeps = initDeps, InitAfterMain = initAfterMain, Main = main}
 end,
+["Notepad"] = function()
+-- Common Locals
+local Main,Lib,Apps,Settings -- Main Containers
+local Explorer, Properties, Notepad, Notebook -- Major Apps
+local API,RMD,env,service,plr,create,createSimple -- Main Locals
 
+local function initDeps(data)
+	Main = data.Main
+	Lib = data.Lib
+	Apps = data.Apps
+	Settings = data.Settings
+
+	API = data.API
+	RMD = data.RMD
+	env = data.env
+	service = data.service
+	plr = data.plr
+	create = data.create
+	createSimple = data.createSimple
+end
+
+local function initAfterMain()
+	Explorer = Apps.Explorer
+	Properties = Apps.Properties
+	Notepad = Apps.Notepad
+	Notebook = Apps.Notebook
+end
+
+local function main()
+	local Notepad = {}
+
+	local window, codeFrame
+
+	Notepad.Init = function()
+		window = Lib.Window.new()
+		window:SetTitle("Notepad")
+		window:Resize(500, 400)
+		Notepad.Window = window
+
+		codeFrame = Lib.CodeFrame.new()
+		codeFrame.Frame.Position = UDim2.new(0, 0, 0, 20)
+		codeFrame.Frame.Size = UDim2.new(1, 0, 1, -20)
+		codeFrame.Frame.Parent = window.GuiElems.Content
+
+		local execute = Instance.new("TextButton", window.GuiElems.Content)
+		execute.BackgroundTransparency = 1
+		execute.Position = UDim2.new(0, 0, 0, 0)
+		execute.Size = UDim2.new(0.25, 0, 0, 20)
+		execute.Text = "Execute"
+		execute.TextColor3 = Color3.new(1, 1, 1)
+
+		execute.MouseButton1Click:Connect(function()
+			local source = codeFrame:GetText()
+			loadstring(source)()
+		end)
+
+		local clear = Instance.new("TextButton", window.GuiElems.Content)
+		clear.BackgroundTransparency = 1
+		clear.Position = UDim2.new(0.25, 0, 0, 0)
+		clear.Size = UDim2.new(0.25, 0, 0, 20)
+		clear.Text = "Clear"
+		clear.TextColor3 = Color3.new(1, 1, 1)
+
+		clear.MouseButton1Click:Connect(function()
+			codeFrame:SetText("")
+		end)
+
+		local copy = Instance.new("TextButton", window.GuiElems.Content)
+		copy.BackgroundTransparency = 1
+		copy.Position = UDim2.new(0.5, 0, 0, 0)
+		copy.Size = UDim2.new(0.25, 0, 0, 20)
+		copy.Text = "Copy to Clipboard"
+		copy.TextColor3 = Color3.new(1, 1, 1)
+
+		copy.MouseButton1Click:Connect(function()
+			local source = codeFrame:GetText()
+			setclipboard(source)
+		end)
+
+		local save = Instance.new("TextButton", window.GuiElems.Content)
+		save.BackgroundTransparency = 1
+		save.Position = UDim2.new(0.75, 0, 0, 0)
+		save.Size = UDim2.new(0.25, 0, 0, 20)
+		save.Text = "Save to File"
+		save.TextColor3 = Color3.new(1, 1, 1)
+
+		save.MouseButton1Click:Connect(function()
+			local source = codeFrame:GetText()
+			local filename = "Place_" .. game.PlaceId .. "_Script_" .. os.time() .. ".txt"
+			writefile(filename, source)
+			if movefileas then
+				movefileas(filename, ".txt")
+			end
+		end)
+	end
+
+	return Notepad
+end
+
+return {InitDeps = initDeps, InitAfterMain = initAfterMain, Main = main}
+end,
 ["Explorer"] = function()
 --[[
 	Explorer App Module
@@ -11342,7 +11442,7 @@ end
 }
 
 -- Main vars
-local Main, Explorer, Properties, ScriptViewer, Console, DefaultSettings, Notebook, Serializer, Lib
+local Main, Explorer, Properties, ScriptViewer, Notepad, Console, DefaultSettings, Notebook, Serializer, Lib
 local API, RM
 
 -- Default Settings
@@ -11455,7 +11555,7 @@ end
 Main = (function()
 	local Main = {}
 	
-	Main.ModuleList = {"Explorer","Properties","ScriptViewer","Console"}
+	Main.ModuleList = {"Explorer","Properties","ScriptViewer","Notepad","Console"}
 	Main.Elevated = false
 	Main.MissingEnv = {}
 	Main.Version = "" -- pre-aplha ( in-dev )
@@ -11540,11 +11640,13 @@ Main = (function()
 		Properties = Apps.Properties
 		ScriptViewer = Apps.ScriptViewer
 		Console = Apps.Console
+        Notepad = Apps.Notepad
 		Notebook = Apps.Notebook
 		local appTable = {
 			Explorer = Explorer,
 			Properties = Properties,
 			ScriptViewer = ScriptViewer,
+            Notepad = Notepad,
 			Console = Console,
 			Notebook = Notebook
 		}
@@ -12257,6 +12359,8 @@ Main = (function()
 		
 		Main.CreateApp({Name = "Script Viewer", IconMap = Main.LargeIcons, Icon = "Script_Viewer", Window = ScriptViewer.Window})
 		
+		Main.CreateApp({Name = "Notepad", IconMap = Main.LargeIcons, Icon = "Script_Viewer", Window = Notepad.Window})
+
 		Main.CreateApp({Name = "Console", IconMap = Main.LargeIcons, Icon = "Output", Window = Console.Window})
 		
 		local cptsOnMouseClick = nil
@@ -12395,6 +12499,7 @@ Main = (function()
 		Explorer.Init()
 		Properties.Init()
 		ScriptViewer.Init()
+        Notepad.Init()
 		Console.Init()
 		Lib.FastWait()
 		
