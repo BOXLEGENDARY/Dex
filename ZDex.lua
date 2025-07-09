d73d6062ba99093c7e8d5231b24207dfe3872b22
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-07-09 18:03:56 +0700

    Add files via upload

--- ZDex.lua
+++ ZDex.lua
@@ -9756,16 +9756,11 @@ local function main()
 			--self.TextChanged:Fire()
 		end
 		
-		funcs.ConvertText = function(self, text, toEditor)
-			local tabString = (Settings and Settings.Editor and Settings.Editor.TabString)
-			if type(tabString) ~= "string" then
-				tabString = "    "
-			end
-		
+		funcs.ConvertText = function(self,text,toEditor)
 			if toEditor then
-				return text:gsub("\t", tabString)
+				return text:gsub("\t",(" %s%s "):format(tabSub,tabSub))
 			else
-				return text:gsub(tabString, "\t")
+				return text:gsub((" %s%s "):format(tabSub,tabSub),"\t")
 			end
 		end
 
