370cca79e6e617bc96777fe01d16be2bdbd34e42
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-07-16 22:19:37 +0700

    Add files via upload

--- ZDex.lua
+++ ZDex.lua
@@ -6198,9 +6198,11 @@ local function main()
 		function BEventMain(Event)
 		    Logger[Event] = 0
 		    Event.Event:Connect(function(...)
-		        if Logger[Event] > Limits[Event.ClassName] then
-		            return
-		        end
+				Logger[Event] = Logger[Event] or 0
+				local limit = Limits[Event.ClassName] or math.huge
+				if Logger[Event] > limit then
+				    return
+				end
 		        Logger[Event] = Logger[Event] + 1
 		        local StrArgs = tableloop({...})
 		        local FullData = string.format(
