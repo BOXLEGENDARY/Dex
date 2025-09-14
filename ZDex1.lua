8b4328e7af5ea827fff40c493c08daa342e8d3d9
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-09-14 12:52:04 +0700

    Add files via upload

--- ZDex1.lua
+++ ZDex1.lua
@@ -14511,7 +14511,6 @@ Main = (function()
 				env.getscriptbytecode = getscriptbytecode
 				env.setfflag = setfflag
 				env.request = (syn and syn.request) or (http and http.request) or (http_request) or (request)
-                env.decompile = decompile
 				env.protectgui = protect_gui or (syn and syn.protect_gui)
 				env.gethui = gethui or get_hidden_gui
 				env.setclipboard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
