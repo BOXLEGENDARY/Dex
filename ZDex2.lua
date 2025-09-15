eb80f5b256c32a39be6c0a7336e179207cdb12be
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-09-16 01:35:00 +0700

    Add files via upload

--- ZDex2.lua
+++ ZDex2.lua
@@ -14222,21 +14222,45 @@ Main = (function()
 	
 	Main.LuauDecompile = function()
 		local Decompile do
-			local Decompile_Source = assert(game:HttpGet("https://raw.githubusercontent.com/BOXLEGENDARY/LuauDecompile/refs/heads/main/init.lua", true))
-		
-			xpcall(function()
-				loadstring(
-					Decompile_Source:gsub(
-						"return %(x %% 2^32%) // %(2^disp%)",
-						"return math.floor((x %% 2^32) / (2^disp))",
-						1
-					),
-					"LuauDecompile"
-				)()
-			end, warn)
-		
-			local _ENV = (getgenv and getgenv()) or (getfenv and getfenv(1)) or _ENV
-			Decompile = _ENV.decompile
+			local Success, Decompile_Source = pcall(function()
+				return game:HttpGet("https://raw.githubusercontent.com/BOXLEGENDARY/LuauDecompile/refs/heads/main/init.lua", true)
+			end)
+			
+			if Success then
+				local CONSTANTS = [[
+		local ENABLED_REMARKS = {
+			COLD_REMARK = false,
+			INLINE_REMARK = false -- currently unused
+		}
+		------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		local DECOMPILER_TIMEOUT = 2 -- seconds
+		local READER_FLOAT_PRECISION = 7 -- up to 99
+		local DECOMPILER_MODE = "disasm" -- disasm/optdec
+		local SHOW_DEBUG_INFORMATION = true -- show trivial function and array allocation details
+		local SHOW_INSTRUCTION_LINES = true -- show lines as they are in the source code
+		local SHOW_OPERATION_NAMES = true
+		local SHOW_OPERATION_INDEX = true -- show instruction index. used in jumps #n.
+		local SHOW_TRIVIAL_OPERATIONS = true
+		local USE_TYPE_INFO = true -- allow adding types to function parameters (ex. p1: string, p2: number)
+		local LIST_USED_GLOBALS = true -- list all (non-Roblox!!) globals used in the script as a top comment
+		local RETURN_ELAPSED_TIME = true -- return time it took to finish processing the bytecode
+		local DECODE_AS_BASE64 = false -- Decodes the bytecode as base64 if it's returned as such.
+		local USE_IN_STUDIO = false -- Toggles Roblox Studio mode, which allows for this to be used in
+		------------------------------------------------------------------------------------------------------------------------------------------------------------------------]]
+				
+				xpcall(function()
+					return loadstring(
+						string.gsub(
+							string.gsub(
+								Decompile_Source, "return %(x %% 2^32%) // %(2^disp%)", "return math.floor((x %% 2^32) / (2^disp))", 1
+							), ";;CONSTANTS HERE;;", CONSTANTS
+						), "LuauDecompile"
+					)()
+				end, warn)
+				
+				local _ENV = (getgenv or getrenv or getfenv)()
+				Decompile = _ENV.decompile
+			end
 		end
 	end
 
