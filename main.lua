ef103ef87ae607c5b792438ac169adc4ccd6fc5d
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2026-02-08 20:39:18 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -763,39 +763,7 @@ Main = (function()
 	end
 
 	Main.LoadAdvancedLuauDecompiler = function()
-	    local Success, Decompile_Source = pcall(function()
-	        return game:HttpGet("https://raw.githubusercontent.com/BOXLEGENDARY/Advanced-Luau-Decompiler/main/init.lua", true)
-	    end)
-	    
-	    if Success then
-			local CONSTANTS = [[
-	local ENABLED_REMARKS = {
-		COLD_REMARK = false,
-		INLINE_REMARK = false -- currently unused
-	}
-	local DECOMPILER_TIMEOUT = 2 -- seconds
-	local READER_FLOAT_PRECISION = 7 -- up to 99
-	local DECOMPILER_MODE = "disasm" -- disasm/optdec
-	local SHOW_DEBUG_INFORMATION = true -- show trivial function and array allocation details
-	local SHOW_INSTRUCTION_LINES = true -- show lines as they are in the source code
-	local SHOW_OPERATION_NAMES = true
-	local SHOW_OPERATION_INDEX = true -- show instruction index. used in jumps #n.
-	local SHOW_TRIVIAL_OPERATIONS = true
-	local USE_TYPE_INFO = true -- allow adding types to function parameters (ex. p1: string, p2: number)
-	local LIST_USED_GLOBALS = true -- list all (non-Roblox!!) globals used in the script as a top comment
-	local RETURN_ELAPSED_TIME = true -- return time it took to finish processing the bytecode
-	local DECODE_AS_BASE64 = false -- Decodes the bytecode as base64 if it's returned as such.
-	local USE_IN_STUDIO = false -- Toggles Roblox Studio mode, which allows for this to be used in]]
-	        
-	        local code = Decompile_Source:gsub(";;CONSTANTS HERE;;", CONSTANTS)
-	        code = code:gsub("return %%(x %%%% 2%^32%%) // %%(2%^disp%%)", "return math.floor((x %% 2^32) / (2^disp))")
-	        
-	        local f, err = loadstring(code, "Advanced-Luau-Decompiler")
-	        if f then
-	            setfenv(f, getgenv())
-	            pcall(f)
-	        end
-	    end
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/BOXLEGENDARY/Advanced-Luau-Decompiler/main/init.lua", true))()
 	end
 
 	Main.ShowGui = Main.SecureGui
