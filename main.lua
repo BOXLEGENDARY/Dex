5dfb1f345d7baaf4e0cc8ee88838477a0e10e9e4
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2026-03-22 08:16:40 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,36 +1,37 @@
 local oldgame = oldgame or game
 
 cloneref = cloneref or function(ref)
-	if not getreg then return ref end
-	
-	local InstanceList
-	
-	local a = Instance.new("Part")
-	for _, c in pairs(getreg()) do
-		if type(c) == "table" and #c then
-			if rawget(c, "__mode") == "kvs" then
-				for d, e in pairs(c) do
-					if e == a then
-						InstanceList = c
-						break
-					end
-				end
-			end
-		end
-	end
-	local f = {}
-	function f.invalidate(g)
-		if not InstanceList then
-			return
-		end
-		for b, c in pairs(InstanceList) do
-			if c == g then
-				InstanceList[b] = nil
-				return g
-			end
-		end
-	end
-	return f.invalidate
+    if not getreg then return ref end
+    
+    local InstanceList
+    local a = Instance.new("Part")
+    
+    for _, c in pairs(getreg()) do
+        if type(c) == "table" then
+            if rawget(c, "__mode") == "kvs" then
+                for d, e in pairs(c) do
+                    if e == a then
+                        InstanceList = c
+                        break
+                    end
+                end
+            end
+        end
+        if InstanceList then break end
+    end
+    
+    a:Destroy()
+
+    if InstanceList then
+        for b, c in pairs(InstanceList) do
+            if c == ref then
+                InstanceList[b] = nil
+                break
+            end
+        end
+    end
+
+    return ref 
 end
 
 -- Main vars
